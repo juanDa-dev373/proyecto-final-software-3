fafab496b15b9e406764a1503383808e7a63561e
juanDa-dev373 <teamcracks38@gmail.com>
2024-10-14 21:40:04 -0500

    primer commit por juan david rios fonseca

---
+++ src/main/java/co/gestiondetareas/sportifypro/SportifyProApplication.java
@@ -0,0 +1,13 @@
+package co.gestiondetareas.sportifypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportifyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportifyProApplication.class, args);
+    }
+
+}
